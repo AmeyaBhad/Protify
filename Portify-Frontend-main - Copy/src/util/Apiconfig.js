f2dbf0f0fd795d3394289fda1fdaead76f2c3999
AmeyaBhad <116884689+AmeyaBhad@users.noreply.github.com>
2026-02-06 13:13:30 +0530

    Delete Portify-Frontend-main - Copy directory

--- Portify-Frontend-main - Copy/src/util/Apiconfig.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'http://localhost:5000/api';
-
-const Apiconfig = {
-  login: `${BASE_URL}/auth/login`,
-  register: `${BASE_URL}/auth/register`,
-};
-
-export default Apiconfig; 
--- Portify-Frontend-main - Copy/src/util/Apiconfig.jsx
+++
@@ -1,9 +0,0 @@
-const PORT = 3030;
-
-class ApiConfig {
-  static login = `http://localhost:${PORT}/api/auth/login`;
-  static register = `http://localhost:${PORT}/api/auth/register`;
-  static logout = `http://localhost:${PORT}/api/auth/logout`;
-}
-
-export default ApiConfig;
